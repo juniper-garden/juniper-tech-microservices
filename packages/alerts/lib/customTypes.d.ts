6ef9a5cf59b6f690d0cb1735c956f0f10d01829a
Daniel <daniel.ashcraft@ofashandfire.com>
2022-03-27 16:33:01 -0500

    Updated to include some varous types and enums.

--- packages/alerts/lib/customTypes.d.ts
+++ packages/alerts/lib/customTypes.d.ts
@@ -1,3 +1,26 @@
 export type Hash = {
   [key: string]: any;
 }
+
+export type Alert = {
+  id: string;
+  name: string;
+  alert_type: AlertType;
+  status: AlertStatus;
+  json_rule: string;
+}
+
+export type AlertRuleType = 'sms' | 'email' | 'desktopPushNofication' | 'mobilePushNotification';
+
+// Alert type is meant to differentiate priority of alerts
+// generic means this is not a critical alert
+// in the future this may go away or be incorporated into another model
+enum AlertType {
+  0 = 'generic'
+}
+
+enum AlertStatus {
+  0 = 'active',
+  1 = 'archived',
+  2 = 'removed'
+}
